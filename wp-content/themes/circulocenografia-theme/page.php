53803ba9e3768b24534ffa53550b94bb584f689a
Alex Koti <alexkoti@gmail.com>
2015-05-17 20:13:56 -0300

    Limpeza no template page.php

--- wp-content/themes/circulocenografia-theme/page.php
+++ wp-content/themes/circulocenografia-theme/page.php
@@ -4,7 +4,7 @@
  */
 
 get_header(); ?>
-		
+
 <div class="container">
 	<div class="row row-offcanvas row-offcanvas-left">
 		<div class="col-md-3 col-sm-4 col-xs-6 sidebar-offcanvas" id="offcanvas-sidebar">
@@ -13,7 +13,6 @@ get_header(); ?>
 		<div class="col-md-9 col-sm-8 col-xs-12" id="offcanvas-content">
 			<?php
 			if (have_posts()){
-				custom_content_nav( 'nav_above' );
 				while (have_posts()){
 					the_post();
 					get_template_part( 'content', 'page' );
